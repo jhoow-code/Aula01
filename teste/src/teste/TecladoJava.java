261f093d2488656bd37bad68b019b6127482b5fb
jhona <jhona@JhoowMartins>
2023-11-28 12:39:07 -0300

    ola mundo e teclado

---
+++ teste/src/teste/TecladoJava.java
@@ -0,0 +1,15 @@
+package teste;
+import java.util.Scanner;
+
+public class TecladoJava {
+	public static void main(String []args) {
+			Scanner scanner = new Scanner(System.in);
+			System.out.print("Digite seu nome:");
+			String nome = scanner.nextLine();
+			System.out.print("Digite sua Idade;");
+			int idade = scanner.nextInt ();
+			System.out.println("Nome: " + nome);
+			System.out.println("Idade: " + idade);
+			scanner.close();
+	}
+}
